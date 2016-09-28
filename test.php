2d09756c034bd99200c97b17a7ae039c663ece0e
jdpedrie <john@pedrie.com>
2016-09-28 11:13:47 -0400

    big

---
+++ test.php
@@ -0,0 +1,16 @@
+<?php
+
+include "vendor/autoload.php";
+
+$cloud = new ServiceBuilder();
+$ds = $cloud->datastore();
+
+$key = $ds->key('Travis', time());
+
+$ds->upsert($ds->entity($key, [
+    'firstName' => 'Travis',
+    'lastName' => 'CI',
+    'time' => time()
+]));
+
+$e = $ds->lookup($key);
